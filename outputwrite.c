c8bac4387a670a5663d0fac25fe9552a924aba90
AVartanyan <armo_ltd@me.com>
2026-07-07 22:38:46 +0300

    v0.1.0
    Initial Release

---
+++ outputwrite.c
@@ -0,0 +1,244 @@
+/*
+ MFi: 518311
+ Brand: APRIMS
+ Project: Relayt
+ 
+ Created by Aram Vartanyan, (C) 2024
+ */
+
+#include <stdio.h>
+#include <esp_system.h>
+#include <esp_log.h>
+#include "outputwrite.h"
+
+const static char *TAG = "InputOutput";
+
+//https://docs.espressif.com/projects/esp-idf/en/v5.2.1/esp32/api-reference/peripherals/gpio.html
+//Може да се направи по друг начин. Едното инициализиране да е за входове, а другото за изходи. Така ще се поддържат всички функции.
+
+esp_err_t ioInit(gpio_num_t keyPinNumber, bool isOutput, bool isPullUp) {
+    
+    gpio_config_t io_conf;
+
+    io_conf.pin_bit_mask = 1ULL << keyPinNumber;
+    io_conf.intr_type = GPIO_INTR_DISABLE; //GPIO_INTR_DISABLE or GPIO_INTR_ANYEDGE
+    
+    if (isOutput) {
+        io_conf.mode = GPIO_MODE_OUTPUT;
+    } else {
+        io_conf.mode = GPIO_MODE_INPUT;
+    }
+    
+    if (isPullUp) {
+        io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    } else {
+        io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    }
+    
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    
+    return gpio_config(&io_conf);
+}
+
+esp_err_t ioInitPdown(gpio_num_t keyPinNumber, bool isOutput, bool isPullDwn) {
+    
+    gpio_config_t io_conf;
+
+    io_conf.pin_bit_mask = 1ULL << keyPinNumber;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    
+    if (isOutput) {
+        io_conf.mode = GPIO_MODE_OUTPUT;
+    } else {
+        io_conf.mode = GPIO_MODE_INPUT;
+    }
+    
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    
+    if (isPullDwn) {
+        io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    } else {
+        io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    }
+    
+    return gpio_config(&io_conf);
+}
+
+int ReadInput(gpio_num_t keyPinNumber) {
+    return gpio_get_level(keyPinNumber);
+}
+
+void OutputWrite(bool on, gpio_num_t keyPinNumber, bool isReverse) {
+    
+    if (isReverse) {
+        gpio_set_level(keyPinNumber, on ? 0 : 1);
+    } else {
+        gpio_set_level(keyPinNumber, on ? 1 : 0);
+    }
+}
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+
+//ADC Calibration
+
+bool adcCalibrationInit(adc_unit_t Unit, adc_channel_t Channel, adc_atten_t Attenuation, adc_oneshot_unit_handle_t *adcHandle, adc_cali_handle_t *adcCalibrationHandle) {
+    
+    adc_cali_handle_t Handle = NULL;
+    esp_err_t ret = ESP_FAIL;
+    bool isCalibrated = false;
+    
+    //ADC1 Init
+    adc_oneshot_unit_handle_t adcUnitHandle;
+    adc_oneshot_unit_init_cfg_t initConfig = {
+        .unit_id = Unit, //ADC_UNIT_1
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&initConfig, &adcUnitHandle));
+    
+    //ADC1 Config
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT, //max supported width will be selected
+        .atten = Attenuation,
+    };
+    
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adcUnitHandle, Channel, &config));
+    
+    *adcHandle = adcUnitHandle;
+
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    if (!isCalibrated) {
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+        adc_cali_curve_fitting_config_t cali_config = {
+            .unit_id = Unit,
+            .chan = Channel,
+            .atten = Attenuation,
+            .bitwidth = ADC_BITWIDTH_DEFAULT, //max supported width will be selected
+        };
+        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &Handle);
+        if (ret == ESP_OK) {
+            isCalibrated = true;
+        }
+    }
+#endif //ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+
+#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    if (!isCalibrated) {
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+        adc_cali_line_fitting_config_t cali_config = {
+            .unit_id = Unit,
+            .atten = Attenuation,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_line_fitting(&cali_config, &Handle);
+        if (ret == ESP_OK) {
+            isCalibrated = true;
+        }
+    }
+#endif //ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+
+    *adcCalibrationHandle = Handle;
+    if (ret == ESP_OK) {
+        ESP_LOGI(TAG, "Calibration Success");
+    } else if (ret == ESP_ERR_NOT_SUPPORTED || !isCalibrated) {
+        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+    } else {
+        ESP_LOGE(TAG, "Invalid arg or no memory");
+    }
+
+    return isCalibrated;
+}
+
+uint32_t adcRead(adc_oneshot_unit_handle_t adcHandle, adc_channel_t Channel, bool isCalibrated, adc_cali_handle_t adcCalibrationHandle) {
+    int RawReading = 0;
+    int Voltage = 0;
+
+    ESP_ERROR_CHECK(adc_oneshot_read(adcHandle, Channel, &RawReading));
+    ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, Channel, RawReading);
+    if (isCalibrated) {
+        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adcCalibrationHandle, RawReading, &Voltage));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, Channel, Voltage);
+    }
+    return Voltage;
+}
+
+static void adcCalibrationDeinit(adc_cali_handle_t Handle) {
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(Handle));
+
+#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(Handle));
+#endif //ADC_CALI_SCHEME
+}
+
+//isCalibrated is the return of the adcCalibrationInit function
+void adcDeinit(adc_oneshot_unit_handle_t adcHandle, bool isCalibrated, adc_cali_handle_t adcCalibrationHandle) {
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(adcHandle));
+    if (isCalibrated) {
+        adcCalibrationDeinit(adcCalibrationHandle);
+    }
+}
+
+#else //if the esp-idf version is below 5.0
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0)
+
+#define DEFAULT_VREF    1100
+
+//esp32 - ADC_BITWIDTH_9, ADC_BITWIDTH_10, ADC_BITWIDTH_11, ADC_BITWIDTH_12
+//esp32s2 ADC_BITWIDTH_13
+//esp32s3 ADC_BITWIDTH_12
+//static const adc_channel_t Channel = ADC_CHANNEL_6;     //GPIO34 if ADC1
+
+esp_adc_cal_characteristics_t adcCalibration(const adc_channel_t Channel, adc_bits_width_t WidthBit, adc_atten_t Attenuation) {
+    
+    static esp_adc_cal_characteristics_t *adc_chars;
+    
+    //Check if Two Point or Vref are burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        ESP_LOGI(TAG, "eFuse Two Point: Supported");
+    } else {
+        ESP_LOGI(TAG, "eFuse Two Point: NOT supported");
+    }
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        ESP_LOGI(TAG, "eFuse Vref: Supported");
+    } else {
+        ESP_LOGI(TAG, "eFuse Vref: NOT supported");
+    }
+
+    //Configure ADC
+    adc1_config_width(WidthBit);
+    adc1_config_channel_atten(Channel, Attenuation); //ADC_ATTEN_DB_0 => 0 to 800 mV, ADC_ATTEN_DB_2_5 => 0 to 1100 mV
+
+    //Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, Attenuation, WidthBit, DEFAULT_VREF, adc_chars);
+    
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        ESP_LOGI(TAG, "Characterized using Two Point Value");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        ESP_LOGI(TAG, "Characterized using eFuse Vref");
+    } else {
+        ESP_LOGI(TAG, "Characterized using Default Vref");
+    }
+    
+    return adc_chars;
+}
+
+uint32_t adcRead(const adc_channel_t Channel, esp_adc_cal_characteristics_t *adc_chars) {
+    uint8_t NoSamples = 5;
+    uint32_t RawReading = 0;
+    
+    for (int i = 0; i < NoSamples; i++) {
+        RawReading += adc1_get_raw((adc1_channel_t)Channel);
+        vTaskDelay(pdMS_TO_TICKS(2));
+    }
+    RawReading /= NoSamples;
+    return esp_adc_cal_raw_to_voltage(RawReading, adc_chars);
+}
+
+#endif
+
+#endif //if the esp-idf version is below 5.0
+
---
+++ outputwrite.h
@@ -0,0 +1,137 @@
+/*
+ MFi: 518311
+ Brand: APRIMS
+ Project: Relayt
+ 
+ Created by Aram Vartanyan, (C) 2024
+ */
+
+#ifndef _OUTPUTWRITE_H_
+#define _OUTPUTWRITE_H_
+
+//#pragma once
+#include <driver/gpio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Set a GPIO Pin for Accessory
+ * @param keyPinNumber GPIO number.
+ * @param isOutput set input/output false / true
+ * @param isPullUp set if the pin is Pulled up false / true
+ * @return
+ * - ESP_OK success
+ * - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t ioInit(gpio_num_t keyPinNumber, bool isOutput, bool isPullUp);
+
+/**
+ * Set a GPIO Pin for Accessory
+ * @param keyPinNumber GPIO number.
+ * @param isOutput set input/output false / true
+ * @param isPullDwn set if the pin is Pulled downt false / true
+ * @return
+ * - ESP_OK success
+ * - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t ioInitPdown(gpio_num_t keyPinNumber, bool isOutput, bool isPullDwn);
+
+/**
+ * @brief  GPIO get input level
+ * @param  gpio_num GPIO number.
+ * @return
+ *     - 0 the GPIO input level is 0
+ *     - 1 the GPIO input level is 1
+ */
+int ReadInput(gpio_num_t keyPinNumber);
+
+/** Drive the general purpose outputs
+ *
+ * @param on - output logical level 1 or 0 -> true or false
+ * @param number of GPIO
+ * @param isReverse true if the ouput logical levels are reversed
+ */
+void OutputWrite(bool on, gpio_num_t keyPinNumber, bool isReverse);
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+
+#include <soc/soc_caps.h>
+#include <esp_adc/adc_oneshot.h>
+#include <esp_adc/adc_cali.h>
+#include <esp_adc/adc_cali_scheme.h>
+
+/**
+ * @brief  Initialize and calibrate ESP ADC
+ * @param  Unit ADC_UNIT_1 or ADC_UNIT_2 (if supported)
+ * @param  Channel ADC_CHANNEL_0 to ADC_CHANNEL_9
+ * @param  Attenuation Check chip documentation
+ * ADC_ATTEN_DB_0   = 0,  ///<No input attenuation, ADC can measure up to approx.
+ * ADC_ATTEN_DB_2_5 = 1,  ///<The input voltage of ADC will be attenuated extending the range of measurement by about 2.5 dB (1.33 x)
+ * ADC_ATTEN_DB_6   = 2,  ///<The input voltage of ADC will be attenuated extending the range of measurement by about 6 dB (2 x)
+ * ADC_ATTEN_DB_11  = 3,  ///<The input voltage of ADC will be attenuated extending the range of measurement by about 11 dB (3.55 x)
+ * @param  adcHandle Output Handle pointer
+ * @param  adcCalibrationHandle Output Calibration Handle pointer
+ * @return
+ *     - 0 is not calibrated
+ *     - 1 calibration is succesful
+ *
+ * Note: On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
+ * Search for errata on espressif website for more details.
+ *
+ * Example:
+ * adc_cali_handle_t OutHandle = NULL; //adcCalibrationHandle
+ * bool isCalibrated = adcCalibrationInit(ADC_UNIT_1, ADC_CHANNEL_2, ADC_ATTEN_DB_11, &OutHandle);
+ */
+bool adcCalibrationInit(adc_unit_t Unit, adc_channel_t Channel, adc_atten_t Attenuation, adc_oneshot_unit_handle_t *adcHandle, adc_cali_handle_t *adcCalibrationHandle);
+
+/**
+ * @brief  Read the ADC in mV
+ * @param  adcHandle Output Handle
+ * @param  Channel ADC_CHANNEL_0 to ADC_CHANNEL_9
+ * @param  isCalibrated bool value return of the adcCalibrationInit function
+ * @param  adcCalibrationHandle Output Calibration Handle
+ * @return Voltage value, mV
+ */
+uint32_t adcRead(adc_oneshot_unit_handle_t adcHandle, adc_channel_t Channel, bool isCalibrated, adc_cali_handle_t adcCalibrationHandle);
+
+/**
+ * @brief  Deinitialize the ADC parameters
+ * @param  adcHandle Output Handle
+ * @param  isCalibrated bool value return of the adcCalibrationInit function
+ * @param  adcCalibrationHandle Output Calibration Handle
+ */
+void adcDeinit(adc_oneshot_unit_handle_t adcHandle, bool isCalibrated, adc_cali_handle_t adcCalibrationHandle);
+
+#else //if the esp-idf version is below 5.0
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0)
+
+#include <esp_adc_cal.h>
+
+/**
+ * @brief  Calibrate ESP ADC in esp-idf 4.x
+ * @param  Channel ADC_CHANNEL_0 to ADC_CHANNEL_9
+ * @param  WidthBit the ADC resolution - example: ADC_BITWIDTH_12
+ * @param  Attenuation Check chip documentation
+ */
+esp_adc_cal_characteristics_t adcCalibration(const adc_channel_t Channel, adc_bits_width_t WidthBit, adc_atten_t Attenuation);
+
+/**
+ * @brief  Read the ADC in esp-idf 4.x
+ * @param  Channel ADC_CHANNEL_0 to ADC_CHANNEL_9
+ * @param  adc_chars ADC pointer returned by adcCalibration function
+ * @return Voltage value, mV
+ */
+uint32_t adcRead(const adc_channel_t Channel, esp_adc_cal_characteristics_t *adc_chars);
+
+#endif
+
+#endif ////if the esp-idf version is below 5.0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _OUTPUTWRITE_H_ */
